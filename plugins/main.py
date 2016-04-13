7c836301473b3bad6c1aded9178d45fc47932611
cory2067 <cjl2625@gmail.com>
2016-04-12 20:25:30 -0400

    added some more useless crap

--- plugins/main.py
+++ plugins/main.py
@@ -15,6 +15,8 @@ outputs = []
 # To submit a response, call say(data, "your message")
 # (have to pass the data variable so it knows which channel to post it to)
 
+names = ['Cory Lynch', 'Jay Laone', 'Kat Yang', 'Mememan', 'Petey', 'Jackie Montante', 'Mayukha Vadari', 'Tafara Mashanda', 'Francisco Proskauer', 'Alejandro Proskauer', 'Joey Noszek', 'Kevin Zheng', 'Eke Wokocha', 'Jason Lam', 'some dude from Venezuela', 'Joaquin Giraldo', 'Connor Sullivan', 'Xiaolu Guo', 'Moin Nadeem', 'Joseph E', 'Jacob Miske', 'McCoy Patino', 'Aleksejs Popovs', 'Slackbot']
+
 
 def choose(letter, size):
 	with open("data/mit/" + letter + ".txt", "r") as f:
@@ -48,13 +50,15 @@ def fortune():
 def process_message(data):
 	txt = str(data['text']).lower()
 	print("Incoming message: " + txt)
+	if randint(0,100) == 42:
+		say(data, 'Lol ' + choice(names) + ' is such an idiot')
 	
 	if 'tell a joke' in txt:
 		r = requests.get("http://tambal.azurewebsites.net/joke/random")
 		say(data, r.json()['joke'])
 	elif 'what' in txt and ("probability" in txt or "chances" in txt):
 		say(data, "My sources indicate a " + str(randint(0,100)) + "% probability")
-	elif any([(txt.startswith(a)) for a in ('where is', 'who is', 'what is', 'what are', 'what do', 'why')]) and (not any([(a in txt) for a in ('this', 'my', 'their', 'you', 'his', 'her', 'our')])) and '?' in txt:
+	elif any([(txt.startswith(a)) for a in ('where is', 'what is', 'what are', 'what do', 'why')]) and (not any([(a in txt) for a in ('this', 'my', 'their', 'you', 'his', 'her', 'our')])) and '?' in txt:
 		#actually sending a request here is pretty useless right now
 		r = requests.get("http://google.com/search", params={"q" : txt})
 		say(data, "Let me Google that for you: " + shorten(r.url))
@@ -64,11 +68,13 @@ def process_message(data):
 		say(data, txt.split('echo')[1])
 	elif 'ayy lmao' in txt:
 		say(data, 'http://put.nu/files/' + choice(('5785U2A.jpg', 'dBSU_J7.gif', 'mW-rnI2.jpg', 'n5rD3GS.jpg', 'DURylFZ.jpg', '5AR4xo8.jpg', 'wnCTLWV.png', 'UD-FMiC.jpg', 'zr-PLxK.jpg')))
-	elif any([txt.startswith(q) for q in ('is', 'are', 'should', 'can', 'does', 'do', 'will', 'has', 'have', 'am')]) and '?' in txt:
-		say(data, choice(('No way.', 'Definitely not.', 'Without a doubt.', 'My sources indicate yes.', 'I have no idea.', 'Idk, ask Chris Peterson', 'What a stupid question. Obviously not.', 'Yes!', 'NOPE.', 'Yeah, I think so.')))
+	elif any([txt.startswith(q) for q in ('is', 'are', 'should', 'can', 'does', 'do', 'will', 'has', 'have', 'am', 'could', 'would')]) and '?' in txt:
+		say(data, choice(('No way.', 'Definitely not.', 'Without a doubt.', 'My sources indicate yes.', 'I have no idea.', 'Idk, ask ' + choice(names), 'What a stupid question. Obviously not.', 'Yes!', 'NOPE.', 'Yeah, I think so, but you should ask ' + choice(names) + ' to be sure.', 'According to ' + choice(names) + ', no.', 'According to ' + choice(names) + ', yes.')))
+	elif txt.startswith('who') and '?' in txt:
+		say(data, choice(names))
 	elif "my fortune" in txt:
 		say(data, fortune())
-	elif "has joined the channel" in txt:
+	elif "has joined [TURNING OFF THIS FEATURE] the channel" in txt:
 		say(data, "Welcome " + txt.split('|')[1].split('>')[0] + "! Here is a one-of-a-kind image we thought you might like. It's based on your personality as interpreted by an algorithm we have been developing over the last few months specifically for this. http://goo.gl/1fkHTt#" + str(randint(1,999999)))
 	elif "random image" in txt:
 		say(data, "Here: http://bit.do/bQdnb#" + str(randint(1,999999)))
@@ -105,16 +111,18 @@ def process_message(data):
 	elif "mit weather" in txt or 'weather at mit' in txt:
 		w = requests.get('http://api.apixu.com/v1/current.json?key=913ed2a5415d4705ab3194612163103&q=02139').json()
 		say(data, "At MIT, it is {0} and {1}F".format(w['current']['condition']['text'], w['current']['temp_f']))
-	elif 'cpw' in txt and 'time' in txt:
-		start = datetime(2016,4,7,9,0)
-		today = datetime.today()
-		d = start - today
-		hours, remainder = divmod(d.seconds, 3600)
-		minutes, seconds = divmod(remainder, 60)
-		milli, micro = divmod(d.microseconds, 1000)
+	elif 'dates' in txt and 'mit' in txt:
+		dates = [('Commit Deadline',datetime(2016,5,2,0,0)), ('FPOPs Begin',datetime(2016,8,23,0,0)), ('Orientation',datetime(2016,8,28,0,0)), ('Classes Begin', datetime(2016,9,7,0,0))]
+		out = ''
+		for date in dates:
+			today = datetime.today()
+			d = date[1] - today
+			hours, remainder = divmod(d.seconds, 3600)
+			minutes, seconds = divmod(remainder, 60)
+			milli, micro = divmod(d.microseconds, 1000)
 
-		say(data,"CPW is in {0} day{6}, {1} hours, {2} minutes, {3} seconds, {4} milliseconds, and {5} microseconds.".format(d.days, hours, minutes, seconds, milli, micro, 's'*bool(d.days-1)))
-		#conditionally shows the 's' for when days is plural, too lazy to add that for the other units of time
+			out += "{event}: {0} day{6}, {1} hours, {2} minutes, {3} seconds, {4} milliseconds, and {5} microseconds.\n".format(d.days, hours, minutes, seconds, milli, micro, 's'*bool(d.days-1), event=date[0])
+		say(data, out)
 	elif (' mit ' in txt) or txt.startswith('mit ') or txt.endswith(' mit') or txt == 'mit':
 		#could probably do something with regex to get rid of those stupid conditions
 		say(data, choose('m', 900) + ' ' + choose('i', 2851) + ' of ' + choose('t', 3575))
